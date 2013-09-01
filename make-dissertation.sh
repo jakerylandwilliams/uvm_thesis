bab63139496c2db259a7177b6739a90da4757f1a
Andy Reagan <github@andyreagan.com>
2013-09-01 18:25:41 -0400

    set up for standard format thesis (not journal article), added scripts for catching common writing errors in bin

--- make-dissertation.sh
+++ make-dissertation.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 pdflatex dissertation;
-for auxfile in chapter*.aux
-do
-    bibtex `basename $auxfile .aux`
-done
 
-for bblfile in chapter*.bbl
-do
-        sed -i 's/thebibliography/references/' $bblfile
-done
+STYLE="Standard"
+
+if [ $STYLE -eq "JA" ]; then
+  for auxfile in chapter*.aux
+  do
+      bibtex `basename $auxfile .aux`
+  done
+  
+  for bblfile in chapter*.bbl
+  do
+          sed -i 's/thebibliography/references/' $bblfile
+  done
+fi
 
 bibtex dissertation;
 pdflatex dissertation;
