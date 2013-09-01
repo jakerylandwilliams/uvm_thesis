0b229dd76a2848a5185c184752772a78143b9571
Andy Reagan <github@andyreagan.com>
2013-09-01 19:52:09 -0400

    moving things around

--- make-dissertation.sh
+++ make-dissertation.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
-pdflatex dissertation;
+
+FILENAME_BASE="doe-dissertation"
 
 # set STYLE to JA for a journal article style thesis
-STYLE="Standard"
+#      (and set the \input's to \include's in the main)
+STYLE="standard"
+
+# first test the body versus our style scripts
+for FILE in $FILENAME_BASE.chapter*-$STYLE.tex
+do
+  echo "checking $FILE"
+  perl bin/lexical-illusion-spotter.pl $FILE
+  bin/weasel-word-spotter.sh $FILE
+  bin/passive-word-spotter.sh $FILE
+done
+
+pdflatex $FILENAME_BASE-main;
 
 if [ $STYLE -eq "JA" ]; then
-  for auxfile in chapter*.aux
+  for auxfile in $FILENAME_BASE.chapter*-JA.aux
   do
       bibtex `basename $auxfile .aux`
   done
   
-  for bblfile in chapter*.bbl
+  for bblfile in $FILENAME_BASE.chapter*-JA.bbl
   do
           sed -i 's/thebibliography/references/' $bblfile
   done
 fi
 
-bibtex dissertation;
-pdflatex dissertation;
-pdflatex dissertation;
-pdflatex dissertation;
+bibtex $FILENAME_BASE-main;
+pdflatex $FILENAME_BASE-main;
+pdflatex $FILENAME_BASE-main;
+pdflatex $FILENAME_BASE-main;
+
+
